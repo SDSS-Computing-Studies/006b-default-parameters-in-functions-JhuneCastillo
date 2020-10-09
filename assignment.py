158838dbceca6f4f1cd43ae2c26eb559149b010d
JhuneCastillo <71406970+JhuneCastillo@users.noreply.github.com>
2020-10-09 12:59:30 -0700

    update

--- assignment.py
+++ assignment.py
@@ -2,18 +2,35 @@
 
 import math
 
-def tempConversion():
+
+def tempConversion(degrees, unit="C"):
+    if unit == "C":
+        converted = 9*(float(degrees)/5)+32
+    elif unit == "F":
+        converted = (5*(float(degrees)-32))/9
+    return round(converted, 1)
 
 
-def factorPair():
-
+def factorPair(number, factor):
+    factors = number/factor
+    answer =[int(factor), int(factors)]
+    answer.sort()
+    return answer
+    
+    
 
 
 def cosineLaw():
+    pass
+
 
 def convertAngle():
+    pass
+
 
 def solution():
+    pass
+
 
 def quadratic():
-
+    pass
